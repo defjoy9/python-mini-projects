6e7be968210447e516c06f7f21d350403858d86e
defjoy <defjoy9@users.noreply.github.com>
2025-03-22 15:24:55 -0400

    fixed isse in challenge 3 to accept spaces and negative numbers

--- easy_challenges.py
+++ easy_challenges.py
@@ -1,31 +1,31 @@
-# Reverse a String - Ask the user for a word and print it in reverse using string indexing.
+# # Reverse a String - Ask the user for a word and print it in reverse using string indexing.
 
 
-word = input("type a word: ")
+# word = input("type a word: ")
 
-print(word[::-1])
+# print(word[::-1])
 
 # Count Vowels in a Sentence - Ask for a sentence and count how many vowels (a, e, i, o, u) it contains.
 
-sentence = input("type a sentence: ").lower()
+# sentence = input("type a sentence: ").lower()
 
-vowels = {
-    "a": 0,
-    "e": 0,
-    "i": 0,
-    "o": 0,
-    "u": 0
-}
-for letter in sentence:
-    if letter in vowels:
-        vowels.update({letter: vowels.get(letter)+1})
+# vowels = {
+#     "a": 0,
+#     "e": 0,
+#     "i": 0,
+#     "o": 0,
+#     "u": 0
+# }
+# for letter in sentence:
+#     if letter in vowels:
+#         vowels.update({letter: vowels.get(letter)+1})
 
-print(vowels)
+# print(vowels)
 
 # Find the Maximum in a List - Ask for a list of numbers and find the largest number without using max().
 
-numbers = input("give me a list of numbers: ")
-max = 0
+numbers = input("give me a list of numbers: ").split()
+max = numbers[0]
 
 
 for digit in numbers:
@@ -36,12 +36,12 @@ print(f"largest number is: {max}")
 
 # Sum of Digits in a Number - Ask for a number and find the sum of its digits.
 
-number = input("give me a number: ")
-total = 0
+# number = input("give me a number: ")
+# total = 0
 
-for digit in number:
-    digit = int(digit)
-    total += digit
+# for digit in number:
+#     digit = int(digit)
+#     total += digit
 
-print(f"total is {total}")
+# print(f"total is {total}")
 
