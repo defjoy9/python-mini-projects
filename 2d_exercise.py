4e72b498c4f5a807b278d5540c6468b1ccdf97d8
defjoy <defjoy9@users.noreply.github.com>
2025-03-27 14:17:29 -0400

    completed exercise 1

---
+++ 2d_exercise.py
@@ -0,0 +1,17 @@
+# exercise 1
+
+numbers = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# print 2d list normally
+for item in numbers:
+    print(item)
+
+#print only the first row
+for item in numbers[0]:
+    print(item)
+
+#print the number in the middle (5)
+print(numbers[1][1])
