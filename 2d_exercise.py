1bd0d3ca5f3017f96ca9f8596c4b4e09899e873a
defjoy <defjoy9@users.noreply.github.com>
2025-03-28 17:45:13 -0400

    completed exercise 3

--- 2d_exercise.py
+++ 2d_exercise.py
@@ -1,30 +1,44 @@
-# exercise 1
+# # exercise 1
 
+# numbers = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# # print 2d list normally
+# for item in numbers:
+#     print(item)
+
+# #print only the first row
+# for item in numbers[0]:
+#     print(item)
+
+# #print the number in the middle (5)
+# print(numbers[1][1])
+
+# # exercise 2 - sum of all numbers in 2d list
+# numbers = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# total = 0
+# for row in numbers:
+#     for item in row:
+#         total += item
+
+# print(f'total is {total}')
+
+# exercise 3 - find the Largest Number in a 2D List
 numbers = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [3, 8, 1],
+    [12, 7, 9],
+    [5, 15, 4]
 ]
-# print 2d list normally
-for item in numbers:
-    print(item)
-
-#print only the first row
-for item in numbers[0]:
-    print(item)
-
-#print the number in the middle (5)
-print(numbers[1][1])
-
-# exercise 2 - sum of all numbers in 2d list
-numbers = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-total = 0
+max_number = numbers[0][0]
 for row in numbers:
     for item in row:
-        total += item
+        if item > max:
+            max_number = item
 
-print(f'total is {total}')
+print(max_number)
